995f3f67f00ff1bda794062c96b6430e51f9be23
Jennyfer-PM <124049932@upq.edu.mx>
2025-10-30 19:10:18 -0600

    Avance

--- AhorraMas/pantallas/PagInicio.js
+++ AhorraMas/pantallas/PagInicio.js
@@ -4,17 +4,26 @@ import React, { Component } from 'react'
 export default function PagInicio(){
     return (
       <View style={styles.container}>
+        <View style={styles.pp}>
         <Text style={styles.titulo}>AHORRO+</Text>
+        <Text style={styles.subtitulo}>Gestiona tu dinero de forma simple e inteligente.</Text>
+        </View>
 
+        <View style={styles.sp}>
         <TouchableOpacity 
         style={styles.boton}>
-          <text style={styles.textoBoton}>Registrame ahora</text>
+          <text style={styles.textoBoton}>Registrarme ahora</text>
         </TouchableOpacity>
 
         <TouchableOpacity 
         style={styles.boton}>
           <text style={styles.textoBoton}>Iniciar sesión</text>
         </TouchableOpacity>
+        </View>
+
+        <View style={styles.tp}>
+        <text style={styles.subtitulo}>Tu presupuesto, bajo control.</text>
+        </View>
 
       </View>
       
@@ -24,16 +33,35 @@ export default function PagInicio(){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
+    backgroundColor: '#007A83',
+  },
+  pp: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#007A83',
+  },
+  sp: {
+    flex: 6,
+    padding: 20,
+    backgroundColor: '#007A83',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  tp: {
     padding: 20,
     backgroundColor: '#007A83',
   },
   boton:{
-    marginTop:40,
+    marginTop: 30,
     flexDirection:'column',
-    gap:40,
-    backgroundColor: '#ffffff'
+    gap: 10,
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+    paddingVertical: 5,
+    paddingHorizontal: 30,
   },
   textoBoton: {
     color: '#007A83',
@@ -41,8 +69,17 @@ const styles = StyleSheet.create({
     fontWeight: '900',
   },
   titulo: {
+    flex: 1,
     color: '#ffffff',
     justifyContent: 'start',
     alignItems: 'start',
-  }
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+  subtitulo: {
+    color: '#ffffff',
+    justifyContent: 'start',
+    alignItems: 'start',
+    fontSize: 20,
+  },
 })
